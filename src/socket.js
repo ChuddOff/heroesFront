c40ec1f8bfbe1952aa738519928e9572b48bffed
Alex <150617937+ChuddOff@users.noreply.github.com>
2024-05-14 20:41:56 +0300

    alfa 4.7

--- src/socket.js
+++ src/socket.js
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
-// const urlDomen = process.env.BACKURL || "https://heroes-back.vercel.app";
-const urlDomen = "http://localhost:4000";
+const urlDomen = process.env.BACKURL || "https://heroes-back.vercel.app";
+// const urlDomen = "http://localhost:4000";
 
 export const socket = io(urlDomen)
